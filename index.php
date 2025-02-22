eeb63eedbd5e2daea599d63037c847b24f26e00b
hayllonprata <102913136+hayllonprata@users.noreply.github.com>
2025-02-22 18:11:54 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Site com Iframe</title>
+    <title>Bit Lion</title>
     <style>
         body {
             background-color: black;
@@ -14,7 +14,6 @@
     </style>
 </head>
 <body>
-    <h1>Meu Site com Iframe</h1>
     <iframe
   src="https://gerenciamento-bitlion-cat.7uwzf6.easypanel.host/proxy.php"
   style="width: 100%; height: 100vh; border: none;"
