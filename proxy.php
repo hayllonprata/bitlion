a1a5183d337d5d32aac16015869d389498c5b6cc
hayllonprata <102913136+hayllonprata@users.noreply.github.com>
2025-02-22 19:54:09 -0300

    up

--- proxy.php
+++ proxy.php
@@ -9,13 +9,5 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3');
 $response = curl_exec($ch);
 curl_close($ch);
-
-// Substituir URLs relativas por absolutas
-$base_url = "https://www.bitcat.com";
-$response = str_replace('href="/', 'href="' . $base_url . '/', $response);
-$response = str_replace('src="/', 'src="' . $base_url . '/', $response);
-$response = str_replace('url(/', 'url(' . $base_url . '/', $response);
-$response = str_replace('href="', 'href="' . $base_url . '/', $response);
-
 echo $response;
 ?>
