c560659783b1c52d5d699aac92bc4203dc8311f2
hayllonprata <102913136+hayllonprata@users.noreply.github.com>
2025-02-22 17:58:05 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<iframe src="https://gerenciamento-bitlion.7uwzf6.easypanel.host/" width="100%" height="500px"></iframe>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site com Iframe</title>
+</head>
+<body>
+    <h1>Meu Site com Iframe</h1>
+    <iframe src="https://gerenciamento-bitlion.7uwzf6.easypanel.host/" width="100%" height="500px"></iframe>
+    
+</body>
+</html>
+
