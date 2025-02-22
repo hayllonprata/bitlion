42572503b3ea0027bd6997740860d48b7a2e095b
hayllonprata <102913136+hayllonprata@users.noreply.github.com>
2025-02-22 19:06:47 -0300

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -1,7 +1,5 @@
 <?php
-$base_url = "https://www.bitcat.com/";
-$url = $base_url . $_SERVER['REQUEST_URI'];
-
+$url = "https://www.bitcat.com/";
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -10,16 +8,13 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3');
 $response = curl_exec($ch);
-
-if(curl_errno($ch)) {
-    echo 'Curl error: ' . curl_error($ch);
-} else {
-    // Substituir URLs relativas por absolutas
-    $response = str_replace('href="/', 'href="' . $base_url . '', $response);
-    $response = str_replace('src="/', 'src="' . $base_url . '', $response);
-    $response = str_replace('url(/', 'url(' . $base_url . '', $response);
-}
-
 curl_close($ch);
+
+// Substituir URLs relativas por absolutas
+$base_url = "https://www.bitcat.com";
+$response = str_replace('href="/', 'href="' . $base_url . '/', $response);
+$response = str_replace('src="/', 'src="' . $base_url . '/', $response);
+$response = str_replace('url(/', 'url(' . $base_url . '/', $response);
+
 echo $response;
 ?>
