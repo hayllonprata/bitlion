e9c0ed6a8f15dccd426f7299de1b8df8243965e5
hayllonprata <102913136+hayllonprata@users.noreply.github.com>
2025-02-22 18:09:40 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
-<!DOCTYPE html>
+<!DOCTYPE html> 
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Site com Iframe</title>
+    <style>
+        body {
+            background-color: black;
+        }
+        iframe {
+            border: none;
+        }
+    </style>
 </head>
 <body>
     <h1>Meu Site com Iframe</h1>
     <iframe src="https://gerenciamento-bitlion-cat.7uwzf6.easypanel.host/proxy.php" width="100%" height="500px"></iframe>
-    
 </body>
 </html>
-
