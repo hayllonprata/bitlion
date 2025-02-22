71102ef018a29bbd96b3ffea28730cf386f2310b
hayllonprata <102913136+hayllonprata@users.noreply.github.com>
2025-02-22 18:05:18 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <h1>Meu Site com Iframe</h1>
-    <iframe src="https://gerenciamento-bitlion-cat.7uwzf6.easypanel.host/" width="100%" height="500px"></iframe>
+    <iframe src="https://gerenciamento-bitlion-cat.7uwzf6.easypanel.host/proxy.php" width="100%" height="500px"></iframe>
     
 </body>
 </html>
