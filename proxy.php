b79f14faa66d2c031a99b4ddf64d43f24fc979e7
hayllonprata <102913136+hayllonprata@users.noreply.github.com>
2025-02-22 21:25:39 -0300

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -1,7 +1,5 @@
 <?php
 $url = "https://www.bitcat.com/";
-
-// Inicializa cURL para buscar a página original
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -9,40 +7,14 @@ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3');
-$html = curl_exec($ch);
+$response = curl_exec($ch);
 curl_close($ch);
 
-// URL base
+// Substituir URLs relativas por absolutas
 $base_url = "https://www.bitcat.com";
+$response = str_replace('href="/', 'href="' . $base_url . '/', $response);
+$response = str_replace('src="/', 'src="' . $base_url . '/', $response);
+$response = str_replace('url(/', 'url(' . $base_url . '/', $response);
 
-// *** 1ª Fase: Modificação via DOMDocument (Abordagem Estruturada) ***
-libxml_use_internal_errors(true);
-$dom = new DOMDocument();
-$dom->loadHTML($html, LIBXML_NOERROR | LIBXML_NOWARNING);
-$tags = $dom->getElementsByTagName('*');
-
-foreach ($tags as $tag) {
-    if ($tag->hasAttribute('href')) {
-        $href = $tag->getAttribute('href');
-        if (preg_match('/^\/?(home|static)\//i', $href)) { 
-            $tag->setAttribute('href', $base_url . '/' . ltrim($href, '/'));
-        }
-    }
-
-    if ($tag->hasAttribute('src')) {
-        $src = $tag->getAttribute('src');
-        if (preg_match('/^\/?(home|static)\//i', $src)) { 
-            $tag->setAttribute('src', $base_url . '/' . ltrim($src, '/'));
-        }
-    }
-}
-
-$html = $dom->saveHTML();
-
-// *** 2ª Fase: Substituição Manual com Expressões Regulares (Força os Caminhos Relativos) ***
-$html = preg_replace('/(href|src|url)\(["\']?(\/?home\/[^"\')]+)/i', '$1="' . $base_url . '/$2', $html);
-$html = preg_replace('/(href|src|url)\(["\']?(\/?static\/[^"\')]+)/i', '$1="' . $base_url . '/$2', $html);
-
-// Imprime o HTML corrigido
-echo $html;
+echo $response;
 ?>
