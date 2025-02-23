95676d27497ef68b862a38173d2ac150d2a79e95
hayllonprata <102913136+hayllonprata@users.noreply.github.com>
2025-02-23 20:35:25 -0300

    Update Banners.tsx

--- src/components/Banners.tsx
+++ src/components/Banners.tsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
 
 const mainBanners = [
-  "./img/top1.webp",
-  "./img/top2.webp",
-  "./img/top3.webp"
+  "./top1.webp",
+  "./top2.webp",
+  "./top3.webp"
 ];
 
 const staticBanners = {
-  left: "./img/left.webp",
-  right: "./img/right.webp"
+  left: "./left.webp",
+  right: "./right.webp"
 };
 
 export function Banners() {
