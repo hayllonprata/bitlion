393ca194c9401c4a9c27a0bd39a4cfdd490c39f7
hayllonprata <102913136+hayllonprata@users.noreply.github.com>
2025-02-22 21:59:00 -0300

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -1,5 +1,5 @@
 <?php
-$url = "https://m.bitcat.com/";
+$url = "https://m.bitcat.com/en_US/";
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
